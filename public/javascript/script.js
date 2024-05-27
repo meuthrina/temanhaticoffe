d1d7e19d0eb80d826fe243f07ce8cabc9f92c5c0
wolsim <annlzghny@gmail.com>
2024-05-27 11:22:57 +0700

    first commit

---
+++ public/javascript/script.js
@@ -0,0 +1,60 @@
+// Event Navigasi
+const navigationList = document.querySelector("#navMenu");
+const hbgrElement = document.querySelector("#hamburger-menu");
+
+// Ketika Hamburger Menu di Click {
+hbgrElement.onclick = () => {
+  navigationList.classList.toggle("-right-full");
+  navigationList.classList.toggle("right-0");
+};
+
+// Click diluar sidebar untuk menghilangkan sidebar kembali {
+document.addEventListener("click", function (clickIn) {
+  // Jika yang di Click bukan Hamburger Menu dan Sidebar Menu {
+  if (
+    !hbgrElement.contains(clickIn.target) &&
+    !navigationList.contains(clickIn.target)
+  ) {
+    navigationList.classList.remove("right-0");
+    navigationList.classList.add("-right-full");
+  }
+  // Ketika Sidebar aktif dan diresize hingga mencapai medium screen {
+  else if (window.innerWidth >= 768) {
+    navigationList.classList.remove("right-0");
+    navigationList.classList.add("-right-full");
+  }
+});
+
+document.addEventListener("resize", function () {
+  if (window.innerWidth <= 768) {
+    navigationList.classList.remove("right-0");
+    navigationList.classList.add("-right-full");
+  }
+});
+
+// ================================================================================================
+
+// Event Pilihan Kategori Daftar Menu
+window.onscroll = function () {
+  var stickySet = document.getElementById("stickySet");
+  var listMenu = document.getElementById("listMenu");
+  var stickySetParent = stickySet.parentElement;
+  var stickySetOffset = stickySetParent.offsetTop;
+  var underLargeWidth = window.innerWidth < 1024;
+  var x = listMenu.getBoundingClientRect();
+
+  if (x.bottom >= window.innerHeight) {
+    if (window.pageYOffset > stickySetOffset && underLargeWidth) {
+      stickySet.classList.add("fixed");
+      listMenu.classList.add("pt-60");
+    } else if (window.pageYOffset > stickySetOffset) {
+      stickySet.classList.add("fixed");
+    } else {
+      stickySet.classList.remove("fixed");
+      listMenu.classList.remove("pt-60");
+    }
+  } else {
+    stickySet.classList.remove("fixed");
+    listMenu.classList.remove("pt-60");
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Poppins"],
+      },
+      colors: {
+        primary: {
+          100: "#022C22",
+          200: "#059669",
+        },
+        secondary: {
+          100: "#991b1b",
+        },
+      },
+      keyframes: {
+        slideReview: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        animateWaveL: {
+          "0%": { backgroundPositionX: "1000px" },
+          "100%": { backgroundPositionX: "0px" },
+        },
+        animateWaveR: {
+          "0%": { backgroundPositionX: "0px" },
+          "100%": { backgroundPositionX: "1000px" },
+        },
+        typing: {
+          from: { width: 0 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
